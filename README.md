3a496b87973b06150c4c826003b11b33fa9bbeef
mikekeke <>
2024-07-03 10:18:49 +0300

    web-export

---
+++ README.md
@@ -0,0 +1,37 @@
+# TBD
+
+## .so compilation
+
+Workdir: `gdext_lib`
+
+`cargo build`
+
+## WASM compilation
+
+Workdir: `gdext_lib`
+
+```bash
+source /home/mike/dev/mlabs/godot-wallet-project/emsdk/emsdk_env.sh
+```
+
+```bash
+emcc --version && cargo +nightly build -Zbuild-std --target wasm32-unknown-emscripten
+```
+
+## Running export
+
+Workdir: repo root
+
+Do web export (export config is set already).
+
+### If in Nix shell
+
+```bash
+make godot-website
+```
+
+Then go to `http://localhost:8060/index.html`
+
+### If NOT in Nix shell
+
+Need to serve [web-export](./web-export/) with headers analogous to [web-export/serve.py](./web-export/serve.py)
--- gdext_godot/main.gd
+++ gdext_godot/main.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var frames_counter_box = HBoxContainer.new()
+var frames_counter_doc = Label.new()
 var frames_counter = Label.new()
 var main_box = VBoxContainer.new()
 
@@ -9,8 +11,12 @@ func _ready():
 	var fib_res = _FibCalculator._slow_fib(4)
 	prints("Fib res: ", fib_res)
 	
+	frames_counter_doc.text = " (updated every frame via '_process' to see when UI freezes)"
+	frames_counter_box.add_child(frames_counter)
+	frames_counter_box.add_child(frames_counter_doc)
+		
 	add_child(main_box)
-	main_box.add_child(frames_counter)
+	main_box.add_child(frames_counter_box)
 	_add_sync_ui(main_box)
 
 var frames_cnt = 0
@@ -19,13 +25,14 @@ func _process(delta):
 	frames_cnt +=1
 	frames_counter.text = str(frames_cnt)
 
-
 func _add_sync_ui(parent):
 	var container = HBoxContainer.new()
 	var input = LineEdit.new()
+	input.text = "43"
 	var out = Label.new()
+	out.text = "(usually 42-45 is big enough to freeze UI)"
 	var go_fib_btn = Button.new()
-	go_fib_btn.text = "Sync fib"
+	go_fib_btn.text = "Slow syncronous fib"
 	go_fib_btn.pressed.connect(_calc_fib.bind([input, out]))
 	
 	container.add_child(input)
@@ -33,15 +40,10 @@ func _add_sync_ui(parent):
 	container.add_child(out)
 	parent.add_child(container)
 	
-	#add_child(set_seed_btn)
-
+func _add_threaded_ui(parent):
+	pass
 
 func _calc_fib(args):
-	var js = JavaScriptBridge.create_object("Object")
-	prints("js: ", js)
-	js.ff = 1
-
-	
 	var inp = args[0]
 	var outp = args[1]
 	var inp_text = inp.text
